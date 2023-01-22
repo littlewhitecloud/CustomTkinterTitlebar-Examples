7590b447cd5e80e217e6b8175fae8e96461a266a
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-01-22 16:26:55 +0800

    Commit 1.0.1

---
+++ testall/example.py
@@ -0,0 +1,17 @@
+""" Test all window effect """
+from custom import CTT
+from tkinter.ttk import Radiobutton, Entry
+from sv_ttk import set_theme
+
+class Main(CTT):
+	""" Class doc """
+	def __init__(self):
+		""" Class initialiser """
+		super().__init__()
+		"""
+		Here is a list for all window effect:
+		UseTitle
+		UseMaxMin
+		
+		Geometry
+		"""
